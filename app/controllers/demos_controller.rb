1f70366dbfa5da4a99b3fabc3a6e3f16a84bffd2
judahmeek <judah.meek@gmail.com>
2016-05-03 22:57:14 +0000

    Making progress
    I had been under the impression that js.erb views would be rendered for JSON requests
    but it looks like I was wrong.

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -2,7 +2,7 @@ class DemosController < ApplicationController
     def test
         respond_to do |format|
             format.json { render json: {'result'=>"json request successful"} }
-            format.js   { render json: {'result'=>"js request successful"} }
+            format.js   {}
         end
     end
 end
